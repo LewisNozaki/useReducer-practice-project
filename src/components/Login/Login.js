c82b1f6eb1791b8a85021eb30b6c831da2c82b66
Lewis Nozaki <lhnozaki@outlook.com>
2021-07-15 17:19:38 -1000

    updates

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,14 +4,36 @@ import styles from './Login.module.css';
 import Button from '../UI/Button/Button';
 
 // Reducer function
-const emailReducer = (state, action) => {
+const emailReducer = (latestState, action) => {
   if (action.type === "USER_EMAIL_INPUT") {
     return {
       value: action.val,
-      isValid: false
+      isValid: action.val.includes("@")
     };
   }
+
+  if (action.type === "INPUT_BLUR") {
+    return {
+      val: latestState.value,
+      isValid: latestState.value.includes("@")
+    }
+  }
+
+  // default return
+  return {
+    value: "",
+    isValid: null
+  };
 };
+
+const passwordReducer = (latestState, action) => {
+
+
+  return {
+    value: "",
+    isValid: null
+  }
+}
 
 const Login = ({ onLogin }) => {
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -21,23 +43,40 @@ const Login = ({ onLogin }) => {
   // useReducer
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: "",
-    isValid: false
-  },);
+    isValid: null
+  });
+
+  // useReducer
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null
+  })
   
   const emailChangeHandler = e => {
     dispatchEmail({
       type: "USER_EMAIL_INPUT",
       val: e.target.value
     });
+
+    setFormIsValid(e.target.value && enteredPassword.trim().length > 6)
   };
 
   const passwordChangeHandler = e => {
+    dispatchPassword({
+      type: "USER_PW_INPUT",
+      val: e.target.value
+    })
+
     setEnteredPassword(e.target.value);
 
     setFormIsValid(emailState.isValid && enteredPassword.trim().length > 6)
   };
   
-  const validateEmailHandler = () => setEmailIsValid(emailState.isValid);
+  const validateEmailHandler = () => {
+    dispatchEmail({
+      type: "INPUT_BLUR"
+    });
+  };
 
   const validatePasswordHandler = () => setPasswordIsValid(enteredPassword.trim().length > 6);
 
